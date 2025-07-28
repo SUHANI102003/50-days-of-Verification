2725cc05f1815051cd417d085a9af7ba9898db78
SUHANI JAIN <jsuhani000710@gmail.com>
2025-07-28 19:33:17 +0530

    Update typedef_enum_dt.sv

--- DAY 01/typedef_enum_dt.sv
+++ DAY 01/typedef_enum_dt.sv
@@ -39,7 +39,21 @@ module typedef_enum_dt;
     $display("\n total number of days in week type is = %0d\n",day.num());
  
   end
-  
-  
-  
+  //-------------------------------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------------
+  /*
+  // "e_true_false" is a new data-type with two valid values: TRUE and FALSE
+	typedef enum {TRUE, FALSE} e_true_false;
+
+	initial begin
+		// Declare a variable of type "e_true_false" that can store TRUE or FALSE
+		e_true_false  answer;
+
+		// Assign TRUE/FALSE to the enumerated variable
+		answer = TRUE;
+
+		// Display string value of the variable
+		$display ("answer = %s", answer.name);
+	end
+ */ 
 endmodule
