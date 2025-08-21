fd6eb0e516e59b2f015f6e2789b82f25fe731e5c
SUHANI JAIN <jsuhani000710@gmail.com>
2025-08-21 20:44:49 +0530

    Update time_cons_single_comp.sv

--- UVM_DAY 14/time_cons_single_comp.sv
+++ UVM_DAY 14/time_cons_single_comp.sv
@@ -46,3 +46,13 @@ endmodule
 
  // reset, main, configure, shutdown all work sequentially 
 // until reset is completed, main won't start
+
+//--------------------------------------------------------
+/*
+# KERNEL: UVM_INFO @ 0: reporter [RNTST] Running test comp...
+# KERNEL: UVM_INFO /home/runner/testbench.sv(23) @ 0: uvm_test_top [comp] Reset Started
+# KERNEL: UVM_INFO /home/runner/testbench.sv(25) @ 10: uvm_test_top [comp] Reset Completed
+# KERNEL: UVM_INFO /home/runner/testbench.sv(32) @ 10: uvm_test_top [mon] Main Phase Started
+# KERNEL: UVM_INFO /home/runner/testbench.sv(34) @ 110: uvm_test_top [mon] Main Phase Ended
+# KERNEL: UVM_INFO /home/build/vlib1/vlib/uvm-1.2/src/base/uvm_report_server.svh(869) @ 110: reporter [UVM/REPORT/SERVER] 
+*/
